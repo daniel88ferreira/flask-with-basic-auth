a0c17cd16500bff49ddf300a205ec885a03b0a77
Daniel Ferreira <danieltf88@gmail.com>
2022-10-21 00:11:51 +0100

    Add DB model

--- app/__main__.py
+++ app/__main__.py
@@ -1,3 +1,3 @@
-from app.main import main
+from .main import main
 
 main()
--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db = SQLAlchemy(app)
+
+
+class Token(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    token = db.Column(db.String, nullable=False)
+    perm = db.Column(db.String, default='r', nullable=False)
+
+    def __repr__(self):
+        return "<Token for user {}>".format(self.username)
 
 
 @app.route('/')
@@ -9,4 +23,6 @@ def index():
 
 
 def main():
+    with app.app_context():
+        db.create_all()
     app.run()
